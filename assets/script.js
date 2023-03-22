9798ca75b69bb337ec444cf391e24959ab635bae
annabellefosburgh <afosburgh20@gmail.com>
2023-03-21 20:11:50 -0500

    creating variables for the random generation

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
-// Assignment code here
+// Variables assigning special characters, letters, and numbers available.
+var character = ["!", "#", "$", "%","&"]
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// A function for an uppercase letter option.
+var uppercase = function (x) {
+  return x.toUpperCase();
+}
+
+//Variable to call specific choices in order to generate password within user parameters.
+var choices;
+
+var generatePassword() {
+  prompt("Your password can have anywhere from 8 to 128 characters.");
+  if (enter < 8 || enter > 128);
+  prompt("Your input is outside of character parameters");
+}
 
 
 // Get references to the #generate element
